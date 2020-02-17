3a4c373ac13f24a4aaaef04fcb7256b56302b37b
Sergey Morev <morev.s.a@gmail.com>
2020-02-17 22:48:37 +0300

    тестирование функций python

--- test_python.py
+++ test_python.py
@@ -77,3 +77,36 @@ def test_hypot_multiply():
             assert abs(h*h - (a*a + b*b)) < epsilon, f'a={a}, b={b}, h={h}'
 
 
+source_data = ['1', '2', '3', '1', '4', '1', '5', '1', '6', '0']
+def test_filter():
+    # Проверка filter().  Получим только единицы.
+    filtered_1 = list(filter(lambda x: x == '1', source_data))
+    assert len(filtered_1) == 4, f'filtered={filtered_1}'
+    assert [x == '1' for x in filtered_1]
+
+    # Проверка filter(). Получим всё кроме единиц.
+    filtered_2 = list(filter(lambda x: x != '1', source_data))
+    assert len(filtered_2) == 6, f'filtered={filtered_2}'
+    assert [x != '1' for x in filtered_2]
+    assert [isinstance(x, str) for x in filtered_2], f'filtered={filtered_2}'
+
+
+def test_map():
+    # Проверка map(). Переведем все строки в int.
+    mapped_1 = list(map(int, source_data))
+    assert len(mapped_1) == len(source_data), f'mapped={mapped_1}'
+    assert [isinstance(x, int) for x in mapped_1]
+
+
+def test_sorted():
+    # Проверяем sorted. Сортировка по возрастанию.
+    sorted_1 = sorted(source_data)
+    assert len(sorted_1) == len(source_data)
+    assert [sorted_1[i] <= sorted_1[i+1] for i in range(len(source_data)-1)]
+
+    # Проверяем sorted. Сортировка по убыванию.
+    sorted_2 = sorted(source_data, reverse=True)
+    assert len(sorted_1) == len(source_data)
+    assert [sorted_2[i] >= sorted_2[i+1] for i in range(len(source_data)-1)]
+
+
